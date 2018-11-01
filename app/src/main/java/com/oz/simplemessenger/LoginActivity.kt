cbdb1845db2ec996e6697d1cfd7a73cff296561d
Oleg Zadvernyak <oleg.zadvernyak@gmail.com>
2018-11-01 21:05:49 +0200

    login ui

---
+++ app/src/main/java/com/oz/simplemessenger/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.oz.simplemessenger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState == null) supportFragmentManager
+            .beginTransaction()
+            .add(android.R.id.content, LoginFragment.newInstance())
+            .commit()
+    }
+
+}
